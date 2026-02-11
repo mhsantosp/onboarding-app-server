fd08005d451d38368bbb04ae83ca20aa3ef26473
Maria Santos <msantospineda1@gmail.com>
2026-02-11 00:44:51 -0500

    Requisito: CORS para permitir peticiones desde el Frontend

--- src/main/java/com/stefanini/onboarding_app_server/account/AccountController.java
+++ src/main/java/com/stefanini/onboarding_app_server/account/AccountController.java
@@ -3,11 +3,13 @@ package com.stefanini.onboarding_app_server.account;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
--- src/main/java/com/stefanini/onboarding_app_server/customer/CustomerController.java
+++ src/main/java/com/stefanini/onboarding_app_server/customer/CustomerController.java
@@ -3,11 +3,13 @@ package com.stefanini.onboarding_app_server.customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
